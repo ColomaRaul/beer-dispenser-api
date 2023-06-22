3460c2b072ed76894bedf1debba33870c576bf62
Raúl Coloma <rau17cb@gmail.com>
2023-06-22 10:53:12 +0200

    TDD refactor controller parameter bag

--- src/Shared/Infrastructure/Api/AbstractApiController.php
+++ src/Shared/Infrastructure/Api/AbstractApiController.php
@@ -6,7 +6,6 @@ namespace App\Shared\Infrastructure\Api;
 use App\Shared\Application\Command\CommandInterface;
 use App\Shared\Application\Query\QueryInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\ParameterBag;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Messenger\HandleTrait;
 use Symfony\Component\Messenger\MessageBusInterface;
